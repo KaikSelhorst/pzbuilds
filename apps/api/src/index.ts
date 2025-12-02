704753a61574430be57f7a14b1efae675e47c03a
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-12-02 09:19:55 -0400

    refactor: simplify index.ts by removing user fetching logic and updating route imports

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,30 +1,8 @@
 import '@/env'
-import { cacheClient } from '@org/cache'
-import { database } from '@org/database'
 
-import { server } from './infra/http/server'
+import { server } from './infra/http/routes'
 
 const app = server
-  .get('/', async () => {
-    try {
-      const cachedUsers = await cacheClient.get('users')
-
-      if (cachedUsers) {
-        return JSON.parse(cachedUsers)
-      }
-
-      const users = await database.query.users.findMany()
-
-      if (!cachedUsers) {
-        await cacheClient.set('users', JSON.stringify(users), 'EX', 60)
-      }
-
-      return users
-    } catch (error) {
-      console.error(error)
-      return { error: 'Failed to fetch users' }
-    }
-  })
   .get('/health', () => ({
     message: 'OK',
   }))
