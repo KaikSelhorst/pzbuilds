6643165dae94e7cbeb2517ba0d9ddf3bdce9e466
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-12-05 23:51:18 -0400

    feat: implement methods for retrieving and creating mods in ModsRepository and SteamModsRepository

--- packages/database/repositories/steam-mods-repository.ts
+++ packages/database/repositories/steam-mods-repository.ts
@@ -1,14 +1,23 @@
 import { eq } from 'drizzle-orm'
 import type { Database } from '../database'
+import { SteamModEntity } from '../entities'
 import { steamMods } from '../schemas'
 
 export class SteamModsRepository {
-  async getSteamModById(tx: Database, modId: string) {
+  async getModById(tx: Database, modId: string) {
     try {
       const steamMod = await tx.query.steamMods.findFirst({
         where: eq(steamMods.id, modId),
       })
-      return steamMod
+      return steamMod ? new SteamModEntity(steamMod) : undefined
+    } catch {
+      return undefined
+    }
+  }
+  async createMod(tx: Database, mod: SteamModEntity) {
+    try {
+      const newSteamMod = await tx.insert(steamMods).values(mod).returning()
+      return new SteamModEntity(newSteamMod[0])
     } catch {
       return undefined
     }
